65bbb8b7bccbd9d92c1888e2ddf2d7d710344137
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-07-17 13:30:12 +0200

    code cleanup

--- src/slist.h
+++ src/slist.h
@@ -1,3 +1,6 @@
+#ifndef MONITOR_SLIST_H
+#define MONITOR_SLIST_H
+
 #include <stdint.h>
 
 typedef struct _slist_t {
@@ -9,3 +12,5 @@ typedef struct _slist_t {
 void slist_init(slist_t *s, uint32_t length);
 void slist_push(slist_t *s, uint32_t value);
 uint32_t slist_pop(slist_t *s);
+
+#endif
