3a1d7a21091f4ed657f739fa1c8223a60da256d1
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-08 21:28:10 +0200

    rename misc.asm to misc.inc

--- asm/x64/guide.asm
+++ asm/x64/guide.asm
@@ -14,7 +14,7 @@
 ; You should have received a copy of the GNU General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-%include "misc.asm"
+%include "misc.inc"
 
 global asm_guide
 global asm_guide_size
--- asm/x64/tramp.asm
+++ asm/x64/tramp.asm
@@ -14,7 +14,7 @@
 ; You should have received a copy of the GNU General Public License
 ; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-%include "misc.asm"
+%include "misc.inc"
 
 global asm_tramp
 global asm_tramp_size
