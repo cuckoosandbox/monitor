227a34fbde13f4da7993c56c2ebf928d7b1ec0c9
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-16 10:34:02 +0200

    simplify msgbox test

--- test/msgbox.c
+++ test/msgbox.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main()
-{
-    HMODULE module; FARPROC m_monitor_init, m_monitor_hook;
-
 #if __x86_64__
-    module = LoadLibrary("monitor-x64.dll");
+#define MONITOR_DLL "monitor-x64.dll"
 #else
-    module = LoadLibrary("monitor-x86.dll");
+#define MONITOR_DLL "monitor-x86.dll"
 #endif
 
-    m_monitor_init = GetProcAddress(module, "monitor_init");
-    m_monitor_hook = GetProcAddress(module, "monitor_hook");
-
-    m_monitor_init(module);
-    m_monitor_hook();
+int main()
+{
+    LoadLibrary(MONITOR_DLL);
 
     MessageBox(NULL, "Hello", "World", 0);
 }
