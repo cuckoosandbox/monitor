6eaeed4b1abcc664b04af07f9f86482a1d904cc0
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-02-04 18:31:34 +0530

    introduce error reporting upon failure of reading configuration

--- src/config.c
+++ src/config.c
@@ -29,49 +29,52 @@ void config_read(config_t *cfg)
     memset(cfg, 0, sizeof(config_t));
 
     FILE *fp = fopen(config_fname, "rb");
-    if(fp != NULL) {
-        while (fgets(buf, sizeof(buf), fp) != NULL) {
-            // Cut off the newline.
-            char *p = strchr(buf, '\r');
-            if(p != NULL) *p = 0;
-
-            p = strchr(buf, '\n');
-            if(p != NULL) *p = 0;
-
-            // Split key=value.
-            p = strchr(buf, '=');
-            if(p == NULL) continue;
-
-            *p = 0;
-
-            const char *key = buf, *value = p + 1;
-
-            if(strcmp(key, "pipe") == 0) {
-                strcpy(cfg->pipe_name, value);
-            }
-            else if(strcmp(key, "shutdown-mutex") == 0) {
-                strcpy(cfg->shutdown_mutex, value);
-            }
-            else if(strcmp(key, "first-process") == 0) {
-                cfg->first_process = value[0] == '1';
-            }
-            else if(strcmp(key, "startup-time") == 0) {
-                cfg->startup_time = strtoul(value, NULL, 10);
-            }
-            else if(strcmp(key, "host-ip") == 0) {
-                cfg->host_ip = inet_addr(value);
-            }
-            else if(strcmp(key, "host-port") == 0) {
-                cfg->host_port = strtoul(value, NULL, 10);
-            }
-            else if(strcmp(key, "force-sleepskip") == 0) {
-                cfg->force_sleep_skip = value[0] == '1';
-            }
-            else if(strcmp(key, "hashes-path") == 0) {
-                strcpy(cfg->hashes_path, value);
-            }
-        }
-        fclose(fp);
-        DeleteFile(config_fname);
+    if(fp == NULL) {
+        MessageBox(NULL, "Error", "Error fetching configuration file!", 0);
+        return;
     }
+
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        // Cut off the newline.
+        char *p = strchr(buf, '\r');
+        if(p != NULL) *p = 0;
+
+        p = strchr(buf, '\n');
+        if(p != NULL) *p = 0;
+
+        // Split key=value.
+        p = strchr(buf, '=');
+        if(p == NULL) continue;
+
+        *p = 0;
+
+        const char *key = buf, *value = p + 1;
+
+        if(strcmp(key, "pipe") == 0) {
+            strcpy(cfg->pipe_name, value);
+        }
+        else if(strcmp(key, "shutdown-mutex") == 0) {
+            strcpy(cfg->shutdown_mutex, value);
+        }
+        else if(strcmp(key, "first-process") == 0) {
+            cfg->first_process = value[0] == '1';
+        }
+        else if(strcmp(key, "startup-time") == 0) {
+            cfg->startup_time = strtoul(value, NULL, 10);
+        }
+        else if(strcmp(key, "host-ip") == 0) {
+            cfg->host_ip = inet_addr(value);
+        }
+        else if(strcmp(key, "host-port") == 0) {
+            cfg->host_port = strtoul(value, NULL, 10);
+        }
+        else if(strcmp(key, "force-sleepskip") == 0) {
+            cfg->force_sleep_skip = value[0] == '1';
+        }
+        else if(strcmp(key, "hashes-path") == 0) {
+            strcpy(cfg->hashes_path, value);
+        }
+    }
+    fclose(fp);
+    DeleteFile(config_fname);
 }
