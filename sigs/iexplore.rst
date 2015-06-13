0c642b3e5c964b9573e7dd9bc76f02e17f6ff81d
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-06-13 20:25:31 +0200

    64-bit cdocument write instrumentation
    In javascript terms, this means document.write("html") logging.

--- sigs/iexplore.rst
+++ sigs/iexplore.rst
@@ -24,3 +24,48 @@ Parameters::
     *  uint32_t unk3
     ** const wchar_t *type
     *  void *exception
+
+
+CDocument_write
+===============
+
+Signature::
+
+    * Callback: addr
+    * Is success: 1
+    * Library: mshtml
+    * Special: true
+    * Return value: int
+
+Parameters::
+
+    *  void *cdocument
+    *  SAFEARRAY *arr
+
+Middle::
+
+    bson b; char index[8];
+    bson_init_size(&b, mem_suggested_size(4096));
+
+    VARIANT *elements = (VARIANT *) arr->pvData;
+    for (uint32_t idx = 0, jdx = 0; idx < arr->rgsabound[0].cElements;
+            idx++, elements++) {
+        if(elements->vt == VT_BSTR && elements->bstrVal != NULL) {
+            uint32_t length = *(uint32_t *)(
+                (uint8_t *) elements->bstrVal - sizeof(uint32_t));
+
+            our_snprintf(index, sizeof(index), "%d", jdx++);
+            log_wstring(&b, index, elements->bstrVal,
+                length / sizeof(wchar_t));
+        }
+    }
+
+    bson_finish(&b);
+
+Logging::
+
+    z lines &b
+
+Post::
+
+    bson_destroy(&b);
--- src/iexplore.c
+++ src/iexplore.c
@@ -130,3 +130,97 @@ uint8_t *hook_addrcb_COleScript_Compile(hook_t *h, uint8_t *module_address)
         module_address, module_size, eval_code_addr);
 }
 
+#if __x86_64__
+
+static uint8_t *_addr_cdocument_write(
+    uint8_t *module_address, uintptr_t module_size, uintptr_t newline_addr)
+{
+    // Locate 'lea rdx, "\r\n"' instruction. There are a couple of these, so
+    // we have to do a little bit of further selection.
+    for (uint32_t idx = 0; idx < module_size - 20; idx++) {
+        if(memcmp(&module_address[idx], "\x48\x8d\x15", 3) != 0) {
+            continue;
+        }
+
+        uintptr_t addr = (uintptr_t) &module_address[idx] +
+            *(int32_t *)(&module_address[idx] + 3) + 7;
+        if(addr != newline_addr) {
+            continue;
+        }
+
+        // If we scan back to the top of the function then we should find
+        // two relative calls of which one is immediately followed by a test
+        // instruction. This signature is unique among the code paths where
+        // the "\r\n" string is used.
+        for (uint32_t jdx = 0; jdx < 256; jdx++) {
+            // The top is denoted by "push rbx ; sub rsp, 0x20".
+            if(memcmp(&module_address[idx - jdx],
+                    "\xff\xf3\x48\x83\xec", 5) != 0) {
+                continue;
+            }
+
+            // Now locate both call instructions.
+            uint8_t *ptr = &module_address[idx - jdx];
+            uint8_t *addr1 = NULL, *addr2 = NULL;
+
+            while (ptr < &module_address[idx]) {
+                if(*ptr == 0xe8) {
+                    if(addr1 == NULL) {
+                        addr1 = ptr;
+                    }
+                    // Only two call instructions, not more, not less.
+                    else if(addr2 != NULL) {
+                        addr1 = addr2 = NULL;
+                        break;
+                    }
+                    else {
+                        addr2 = ptr;
+                    }
+                }
+
+                ptr += lde(ptr);
+            }
+
+            // The second call is followed by a test instruction.
+            if(addr2 != NULL && addr2[5] == 0x85) {
+                return addr1 + *(int32_t *)(addr1 + 1) + 5;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+#else
+
+static uint8_t *_addr_cdocument_write(
+    uint8_t *module_address, uintptr_t module_size, uintptr_t newline_addr)
+{
+    (void) module_address; (void) module_size; (void) newline_addr;
+    return NULL;
+}
+
+#endif
+
+uint8_t *hook_addrcb_CDocument_write(hook_t *h, uint8_t *module_address)
+{
+    (void) h;
+
+    uint32_t module_size = module_image_size(module_address);
+    uintptr_t newline_addr = 0;
+
+    // Locate a possible address of the "\r\n" string.
+    for (uint32_t idx = 0; idx < module_size - 20; idx++) {
+        if(memcmp(&module_address[idx], L"\r\n", 6) == 0) {
+            newline_addr = (uintptr_t) &module_address[idx];
+
+            uint8_t *ret = _addr_cdocument_write(
+                module_address, module_size, newline_addr);
+            if(ret != NULL) {
+                return ret;
+            }
+        }
+    }
+
+    return NULL;
+}
