0f630ffecd98f580fb6e85aca01742c23b3a28ed
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-10-04 13:35:29 +0200

    unittests for imported dlls and hooked functions
    The adapter unittest shows the GetAdaptersInfo() entry in the logs.
    Before the changes to being able to hook recursively loaded DLLs, calls
    to GetAdaptersInfo() and similar would not show up, rendering their
    hooks useless.
    
    The controlservice unittest is just to show that ControlService is being
    hooked under 64-bit Windows 7, as it can't be hooked at once due to lazy
    loading of DLLs (probably).

---
+++ test/test-adapter.c
@@ -0,0 +1,35 @@
+/*
+Cuckoo Sandbox - Automated Malware Analysis.
+Copyright (C) 2010-2015 Cuckoo Foundation.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// EXTENSION= dll
+/// CFLAGS= -shared -static
+/// LDFLAGS += -liphlpapi
+/// OBJECTS=
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    (void) hModule; (void) dwReason; (void) lpReserved;
+
+    MessageBox(NULL, "Adapter", "Before", 0);
+    GetAdaptersInfo(NULL, NULL);
+    MessageBox(NULL, "Adapter", "After", 0);
+    return TRUE;
+}
